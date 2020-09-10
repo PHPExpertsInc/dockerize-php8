31263be216299a5a3bd9e1f57b2409f1c5035f58
Theodore R. Smith <theodore@phpexperts.pro>
2020-09-10 04:37:04 -0500

    Initial. Based off of phpexperts/dockerize-php v6.1.

---
+++ README.md
@@ -0,0 +1,65 @@
+# Dockerize PHP v8.0!
+
+A utility for rapidly deploying PHP v8.0 for testing against your existing projects.
+
+Includes: 
+ * PHP v8.0 beta 3
+ * Extra extensions:
+   * bcmath
+   * curl
+   * gd
+   * gmp
+   * iconv
+   * mbstring
+   * mysqlnd
+   * pdo_mysql
+   * pdo_pgsql
+   * xml
+   * xmlreader
+   * xmlwriter
+   * zip
+
+It is based off of [**phpexperts/dockerize**](https://github.com/phpexpertsinc/dockerize-php).
+
+# Installation
+
+    composer require --dev phpexperts/dockerize-php8
+    
+    # If you want nginx + PHP-FPM and/or mysql/mariadb/postgres/redis:
+    php vendor/phpexperts/dockerize-php8/install.php
+
+### Installation Video
+
+https://youtu.be/xZxaJcsbrWU
+
+### Configure your PATH
+
+Ensure that your profile PATH includes `./vendor/bin` and that it takes priority over any other directory that may include a php executable:
+
+    PATH=./vendor/bin:$PATH
+
+# Running
+
+Run as normal.
+
+If you want to revert back to your built-in version of PHP, do this:
+
+    export PHP_VERSION=native
+
+It will then use your system-level PHP.
+
+You can also set `PHP_VERSION` to any other major PHP version (5.6-8.0).
+See [**phpexperts/dockerize-php**](https://github.com/phpexpertsinc/dockerize-php) for more.
+
+# About PHP Experts, Inc.
+
+[PHP Experts, Inc.](https://www.phpexperts.pro/), is my consultation company. It's a small company of a half dozen 
+highly skilled Full Stack PHP devs, including myself, whom I place at 1099 positions at other corporations. We fill both 
+long-term positions and, for crazy devs like me, short-term. If you ever wanted to work on a different project/company 
+every few months or even weeks, anywhere in the continental U.S., Europe, or South East Asia, it's fantastic.  
+
+Since 2015, I have set up branches in Las Vegas, Houston, the UK, Dublin, Costa Rica, Colombia, India, and the Philippines. 
+If someone has a work auth in any of those places, we can place you almost anywhere you want. I travel 50% of the time 
+out of choice. All over the world.
+
+Plus, no taxes if you spend 6+ months (or a year, if you're American) out of your country. 
---
+++ install.php
@@ -0,0 +1,251 @@
+#!/bin/env php
+<?php
+
+namespace PHPExperts\Dockerize;
+
+installPHP();
+
+installDockerEngines();
+
+$DIR = __DIR__;
+if (!is_dir('./docker')) {
+    if (!mkdir('./docker')) {
+        alert('Error: Could not create the ./docker directory. Installation aborted.');
+        exit(4);
+    }
+}
+system("cp -r $DIR/docker/web ./docker");
+
+echo "\n";
+alert('Next Steps: 1. You should edit your new .env.');
+alert('Next Steps: 2. You should add ./bin to your $PATH.');
+alert('Get Started: To launch the dockerized app, run: `docker-compose up -d`');
+
+$funding = <<<TEXT
+Did you find this project helpful?
+
+Please consider a $5 donation.
+  - Cashapp:  \$theocode
+  - Patreon:  https://www.patreon.com/hopeseekr
+  - Paypal:   hopeseekr@xmule.ws
+  - Bitcoin:  3C6Jay2TEzAksek2geXtqdDrKV5R5EkoWz
+  - Ethereum: hopeseekr@xmule.ws
+
+
+TEXT;
+
+echo $funding;
+
+function installPHP()
+{
+    $dockerStub = file_get_contents(__DIR__ . '/docker/docker-compose.base.yml');
+    $dockerImages = ['8.0'];
+    $newDockerCompose = '';
+
+    foreach ($dockerImages as $index => $PHP_IMAGE) {
+        $PHP_VERSION = $index > 0 ? str_replace(['-debug', '.'], '', $PHP_IMAGE) : '';
+        $PORT = $index === 0 ? '80' : "80{$PHP_VERSION}";
+
+        $versionStub = <<<YAML
+  web{$PHP_VERSION}:
+    image: phpexperts/web:nginx-php{$PHP_IMAGE}
+    volumes:
+      - .:/var/www:delegated
+      - ./docker/web:/etc/nginx/custom
+    ports:
+      - {$PORT}:80      
+YAML;
+        $newDockerCompose .= $versionStub . "\n\n";
+    }
+
+    $dockerStub = str_replace('{{PHP_STUB}}', $newDockerCompose, $dockerStub);
+    file_put_contents('docker-compose.yml', $dockerStub);
+}
+
+function installDockerEngines()
+{
+    $engines = chooseDockerEngines();
+
+    if ($engines[0] === 'None') {
+        return;
+    }
+
+    foreach ($engines as $engine) {
+        installDockerEngine($engine, __DIR__ . "/engines/$engine");
+    }
+
+    system('cp .env.example .env');
+}
+
+function chooseDockerEngines($path = null, $filtered = [])
+{
+    $path = $path !== null ? $path :  __DIR__ . '/engines';
+    if (!is_dir($path)) {
+        alert("Error: Cannot find '$path' directory.");
+        exit(1);
+    }
+
+    $engines = getDirs($path, false, $filtered);
+    array_unshift($engines, 'None');
+    $selection = '';
+    $selectedChoices = [];
+    while ($selection === '') {
+        echo "Choose engines (e.g., '1 3' for both $engines[1] and $engines[3]):\n";
+        foreach ($engines as $index => $engine) {
+            echo "$index) $engine\n";
+        }
+
+        $selection = getUserInput();
+        $selectedChoices = explode(' ', $selection);
+
+        if (count($selectedChoices) > 1 && in_array(0, $selectedChoices)) {
+            alert("Error: '$engines[0]' must be selected by itself.");
+            $selection = '';
+        }
+
+        $invalidChoices = array_diff($selectedChoices, array_keys($engines));
+
+        if (!empty($invalidChoices)) {
+            alert('Error: Invalid choice(s): ' . implode(', ', $invalidChoices));
+            $selection = '';
+        }
+    }
+
+    $selectedEngines = array_values(array_intersect_key($engines, array_flip($selectedChoices)));
+
+    return $selectedEngines;
+}
+
+/**
+ * @param string $engine
+ * @param string $enginePath
+ */
+function installDockerEngine($engine, $enginePath)
+{
+    if (!is_dir($enginePath)) {
+        alert("Error: Cannot find '$enginePath' directory.");
+        exit(1);
+    }
+
+    echo "Installing the $engine Docker engine...\n";
+    if (is_dir("$enginePath/bin")) {
+        if (!is_dir('./bin')) {
+            if (!mkdir('./bin')) {
+                alert('Error: Could not create the ./bin directory. Installation aborted.');
+                exit(2);
+            }
+        }
+        system("cp $enginePath/bin/* ./bin");
+    }
+    system("cat \"$enginePath/docker-compose.stub.yml\" >> ./docker-compose.yml");
+    system('echo >> ./docker-compose.yml');
+
+    if (file_exists("$enginePath/.env.stub")) {
+        overwriteOrAppendFile("$enginePath/.env.stub", '.env.example');
+    }
+
+}
+
+function alert($message)
+{
+    echo "v v v v v v v v v v v v v v v v v v v v v v v\n";
+    echo "--> $message\n";
+    echo "^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^\n\n";
+
+}
+
+function getUserInput()
+{
+    return trim(fgets(STDIN));
+}
+
+/**
+ * Copyright © 2020 Theodore R. Smith <https://www.phpexperts.pro/>
+ * License: MIT
+ *
+ * @see https://stackoverflow.com/a/61168906/430062
+ *
+ * @param string $path
+ * @param bool   $recursive Default: false
+ * @param array  $filtered  Default: [., ..]
+ * @return array
+ */
+function getDirs($path, $recursive = false, array $filtered = [])
+{
+    if (!is_dir($path)) {
+        alert("Error: $path does not exist.");
+        exit(1);
+    }
+
+    $filtered = array_merge(array('.', '..'), $filtered);
+
+    $dirs = array();
+    $d = dir($path);
+    while (($entry = $d->read()) !== false) {
+        if (is_dir("$path/$entry") && !in_array($entry, $filtered)) {
+            $dirs[] = $entry;
+
+            if ($recursive) {
+                $newDirs = getDirs("$path/$entry");
+                foreach ($newDirs as $newDir) {
+                    $dirs[] = "$entry/$newDir";
+                }
+            }
+        }
+    }
+
+    sort($dirs);
+
+    return $dirs;
+}
+
+function overwriteOrAppendFile($srcFile, $destFile, $resetFirstRun = false)
+{
+    static $firstRun = true;
+    if ($resetFirstRun) {
+        $firstRun = true;
+    }
+
+    if (!file_exists($srcFile)) {
+        alert("Error: Cannot find '$srcFile'.");
+
+        exit(3);
+    }
+
+    if (!file_exists($destFile)) {
+        $srcFile = escapeshellarg($srcFile);
+        $destFile = escapeshellarg($destFile);
+        system("echo '#### Block Added by phpexperts/dockerize ####' >> $destFile");
+        system("cp $srcFile $destFile");
+        $firstRun = false;
+
+        return;
+    }
+
+    $selection = null;
+    $shouldAppend = !$firstRun;
+    while ($firstRun && !in_array($selection, [1, 2])) {
+        echo "Append or overwrite $destFile? (1: Append, 2: Overwrite) ";
+        $selection = getUserInput();
+        $shouldAppend = $selection === '1' ? true : false;
+    }
+
+    if (!$shouldAppend) {
+        $srcFile = escapeshellarg($srcFile);
+        $destFile = escapeshellarg($destFile);
+        system("cp $srcFile $destFile");
+        $shouldAppend = true;
+        $firstRun = false;
+
+        return;
+    }
+
+    $srcFile = escapeshellarg($srcFile);
+    $destFile = escapeshellarg($destFile);
+    system("echo >> $destFile");
+    system("echo '#### Block Added by phpexperts/dockerize ####' >> $destFile");
+    system("cat $srcFile >> $destFile");
+    system("echo '#### End Block ####' >> $destFile");
+
+    $firstRun = false;
+}
